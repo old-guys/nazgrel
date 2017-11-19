ebeaeb95424479eae00d2692780062aa540ca9ab
francis jiang <jhjguxin@gmail.com>
2017-11-19 15:31:06 +0800

    [migrate] change channel#category hard code

--- db/migrate/20171116035653_create_channels.rb
+++ db/migrate/20171116035653_create_channels.rb
@@ -2,7 +2,7 @@ class CreateChannels < ActiveRecord::Migration[5.1]
   def change
     create_table :channels, comment: "渠道" do |t|
       t.string :name, null: false, default: "", comment: "渠道名称"
-      t.integer :categroy, null: false, default: 0, comment: "渠道类型"
+      t.integer :category, null: false, default: 0, comment: "渠道类型"
       t.integer :source, null: false, default: 0, comment: "渠道来源"
 
       t.timestamps
