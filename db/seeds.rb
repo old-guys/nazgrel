82108510c93e84a72108688ec562c80679af3193
francis jiang <jhjguxin@gmail.com>
2017-11-19 23:34:47 +0800

    update seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,4 @@
 
 user = User.where(email: "test@qq.com").first_or_create
 user.update(password: "11111111", phone: "1234567890")
-user.api_key
-
-
-channel = Channel.where(name: "测试总代").first_or_create
-channel_user = ChannelUser.where(email: "test@qq.com").first_or_create
-channel_user.update(password: "11111111", phone: "1234567890", channel: channel)
-channel_user.api_key
+user.api_token
