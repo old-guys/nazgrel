567d465fe7dfb5a20b719215367d908526583407
lra <1261847034@qq.com>
2017-11-24 09:49:30 +0800

    rack cors origins改成可配置

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'ishanggang.com'
+    origins SERVICES_CONFIG['rack_cors']['origins']
 
     resource '*',
       headers: :any,
