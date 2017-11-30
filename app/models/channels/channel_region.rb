99160f9d5acb65001b32ef08ca250b87a4676b06
francis jiang <jhjguxin@gmail.com>
2017-11-30 11:19:15 +0800

    channel_region#channels scope with `-> { normal }`

--- app/models/channels/channel_region.rb
+++ app/models/channels/channel_region.rb
@@ -2,7 +2,7 @@ class ChannelRegion < ApplicationRecord
   has_many :channel_users, dependent: :nullify
 
   has_many :channel_channel_region_maps, dependent: :destroy
-  has_many :channels, through: :channel_channel_region_maps
+  has_many :channels, -> { normal }, through: :channel_channel_region_maps
 
   enum status: {
     normal: 0,
