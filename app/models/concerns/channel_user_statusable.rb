bd0c203f7446f8e06348ea1c5ac409aaf44fc398
francis jiang <jhjguxin@gmail.com>
2017-12-08 14:07:51 +0800

    cache channel_users#my data

--- app/models/concerns/channel_user_statusable.rb
+++ app/models/concerns/channel_user_statusable.rb
@@ -67,44 +67,44 @@ module ChannelUserStatusable
   end
 
   def invite_children_reward_amount
-    if region_manager?
-      return channel_region.channels.map(&:invite_children_reward_amount).sum
-    end
-
     Rails.cache.fetch("channel_user:#{id}:#{role_type}:invite_children_reward:raw", raw: true, expires_in: 3.minutes) {
-      own_shopkeeper.children.grade_platinum.size * 200 + own_shopkeeper.children.grade_platinum.size * 100
+      if region_manager?
+        channel_region.channels.map(&:invite_children_reward_amount).sum
+      else
+        own_shopkeeper.children.grade_platinum.size * 200 + own_shopkeeper.children.grade_platinum.size * 100
+      end
     }
   end
 
   def children_comission_amount
-    if region_manager?
-      return channel_region.channels.map(&:children_comission_amount).sum
-    end
-
     Rails.cache.fetch("channel_user:#{id}:#{role_type}:children_comission:raw", raw: true, expires_in: 3.minutes) {
-      Order.where(user_id: own_shopkeeper.children.select(:user_id)).sales_order.valided_order.sum(:comm) * 0.15
+      if region_manager?
+        channel_region.channels.map(&:children_comission_amount).sum
+      else
+        Order.where(user_id: own_shopkeeper.children.select(:user_id)).sales_order.valided_order.sum(:comm) * 0.15
+      end
     }
   end
 
   def invite_children_amount
-    if region_manager?
-      return channel_region.channels.map(&:invite_children_amount).sum
-    end
-
     Rails.cache.fetch("channel_user:#{id}:#{role_type}:invite_amount:raw", raw: true, expires_in: 3.minutes) {
-      own_shopkeeper.children.size * BigDecimal.new(50)
+      if region_manager?
+        channel_region.channels.map(&:invite_children_amount).sum
+      else
+        own_shopkeeper.children.size * BigDecimal.new(50)
+      end
     }
   end
 
   def indirectly_descendant_amount
-    if region_manager?
-      return channel_region.channels.map(&:indirectly_descendant_amount).sum
-    end
-
     Rails.cache.fetch("channel_user:#{id}:#{role_type}:indirectly_descendant_comission:raw", raw: true, expires_in: 3.minutes) {
-      _rate = own_shopkeeper.indirectly_descendants.size > 1000 ? 0.08 : 0.05
-      _amount = Order.where(user_id: own_shopkeeper.indirectly_descendants.select(:user_id)).sales_order.valided_order.sum(:comm)
-      _amount * _rate
+      if region_manager?
+        channel_region.channels.map(&:indirectly_descendant_amount).sum
+      else
+        _rate = own_shopkeeper.indirectly_descendants.size > 1000 ? 0.08 : 0.05
+        _amount = Order.where(user_id: own_shopkeeper.indirectly_descendants.select(:user_id)).sales_order.valided_order.sum(:comm)
+        _amount * _rate
+      end
     }
   end
 
