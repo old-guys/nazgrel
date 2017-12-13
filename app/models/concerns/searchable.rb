39de2e290c23b7422bbecbcd721d0f359a76ef3e
francis jiang <jhjguxin@gmail.com>
2017-12-13 18:53:44 +0800

    fix dirty search_fields variable within simple_search

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -6,12 +6,18 @@ module Searchable
 
     scope :simple_search, -> (query) {
       _all = all
-      _records = default_scoped.where(
-        "#{search_fields.shift} like ?", "%#{query}%"
-      )
-      search_fields.each{|field|
-        _records = _records.or(default_scoped.where("#{field} like ?", "%#{query}%"))
-      }
+      _search_fields = search_fields.dup
+
+      _records = default_scoped
+
+      if _search_fields.present?
+        _records = _records.where(
+          "#{_search_fields.shift} like ?", "%#{query}%"
+        )
+        _search_fields.each{|field|
+          _records = _records.or(default_scoped.where("#{field} like ?", "%#{query}%"))
+        }
+      end
 
       _all.joins(search_joins).merge(_records)
     }
@@ -21,10 +27,10 @@ module Searchable
   module ClassMethods
     def simple_search_on(fields: , joins: )
       self.search_fields ||= []
-      self.search_fields.concat(Array.wrap(fields)).uniq!
+      self.search_fields = search_fields.concat(Array.wrap(fields)).uniq
 
       self.search_joins ||= []
-      self.search_joins.concat(Array.wrap(joins)).uniq!
+      self.search_joins = search_joins.concat(Array.wrap(joins)).uniq
     end
   end
 end
