dd12cc42c6daf8fa80a7cc8cd03415933e600ce2
francis jiang <jhjguxin@gmail.com>
2017-12-18 11:28:39 +0800

    cache constant_setting response content
    update constant_setting#enum_field content

--- app/controllers/api/web/constant_setting_controller.rb
+++ app/controllers/api/web/constant_setting_controller.rb
@@ -11,10 +11,14 @@ class Api::Web::ConstantSettingController < Api::Web::BaseController
     @data = {
       channel: {
         category: ::Channel.categories_i18n,
-        source: ::Channel.sources_i18n
+        source: ::Channel.sources_i18n,
+        status: ::Channel.statuses_i18n,
       },
       channel_user: {
         role_type: ::ChannelUser.role_types_i18n,
+      },
+      channel_region: {
+        status: ::ChannelRegion.statuses_i18n,
       },
       order: {
         order_status: ::Order.order_statuses_i18n,
@@ -24,8 +28,47 @@ class Api::Web::ConstantSettingController < Api::Web::BaseController
         payed_push: ::Order.payed_pushes_i18n,
         global_freight_flag: ::Order.global_freight_flags_i18n,
       },
+      order_detail: {
+        is_free_delivery: ::OrderDetail.is_free_deliveries_i18n,
+        product_label_type: ::OrderDetail.product_label_types_i18n,
+      },
+      order_sub: {
+        order_status: ::OrderSub.order_statuses_i18n,
+        shop_user_deliveried_push: ::OrderSub.shop_user_deliveried_pushes_i18n,
+        user_deliveried_push: ::OrderSub.user_deliveried_pushes_i18n,
+        supplier_deliveried_push: ::OrderSub.supplier_deliveried_pushes_i18n,
+        is_zone_freight: ::OrderSub.is_zone_freights_i18n,
+      },
+      product: {
+        status: ::Product.statuses_i18n,
+        is_pinkage: ::Product.is_pinkages_i18n,
+        label_type: ::Product.label_types_i18n
+      },
+      product_shop: {
+        status: ::ProductShop.statuses_i18n,
+      },
+      income_record: {
+        source_user_level: ::IncomeRecord.source_user_levels_i18n,
+        income_type: ::IncomeRecord.income_types_i18n,
+        status: ::IncomeRecord.statuses_i18n,
+      },
       shopkeeper: {
         user_grade: ::Shopkeeper.user_grades_i18n
+      },
+      shop_user: {
+        sex: ::ShopUser.sexes_i18n,
+        verify_flag: ::ShopUser.verify_flags_i18n,
+        status: ::ShopUser.statuses_i18n,
+        source: ::ShopUser.sources_i18n,
+        shopkeeper_flag: ::ShopUser.shopkeeper_flags_i18n,
+        wechat_flag: ::ShopUser.wechat_flags_i18n,
+      },
+      shop_wechat_user: {
+        gender: ::ShopWechatUser.genders_i18n,
+        status: ::ShopWechatUser.statuses_i18n
+      },
+      supplier: {
+        status: ::Supplier.statuses
       }
     }
   end
--- app/views/api/web/constant_setting/enum_field.json.jbuilder
+++ app/views/api/web/constant_setting/enum_field.json.jbuilder
@@ -1,3 +1,7 @@
-json.(@data, *@data.keys)
+_md5 = Digest::MD5.hexdigest(@data.to_s)
 
-json.md5 Digest::MD5.hexdigest(@data.to_s)
+json.cache! ['api/web/constant_setting/enum_field', _md5], expires_in: 60.minutes do
+  json.(@data, *@data.keys)
+
+  json.md5 _md5
+end
