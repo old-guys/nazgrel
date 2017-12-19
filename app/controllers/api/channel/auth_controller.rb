911aa5f533ef91e9318c71ed474cef8b7644d50d
francis jiang <jhjguxin@gmail.com>
2017-12-19 12:00:35 +0800

    fix api/channel/auth channel, channel_region status check

--- app/controllers/api/channel/auth_controller.rb
+++ app/controllers/api/channel/auth_controller.rb
@@ -6,9 +6,31 @@ class Api::Channel::AuthController < Api::Channel::BaseController
 
     login_field = params[:login].include?("@") ? :email : :phone
     @channel_user = ChannelUser.with_database_authentication(login_field => params[:login]).first
+    @channel = @channel_user.try(:channel)
+    @channel_region = @channel_user.try(:channel_region)
 
     if @channel_user && @channel_user.valid_password?(params[:password])
-      raise Errors::AuthLockError.new("你的帐号已被冻结") if @channel_user.access_locked?
+      if @channel_user.try(:deleted?)
+        raise Errors::UserAuthenticationError.new("该用户已经被删除")
+      end
+
+      if @channel_user.try(:access_locked?)
+        raise Errors::UserAuthenticationError.new("该用户已经被冻结")
+      end
+
+      if @channel_user.region_manager?
+        if not @channel_region.try(:normal?)
+          raise Errors::UserAuthenticationError.new("该用户的渠道管理是无效的")
+        end
+      else
+        unless @channel
+          raise Errors::UserAuthenticationError.new("该用户的渠道是无效的")
+        end
+        unless @channel.normal?
+          raise Errors::UserAuthenticationError.new("该用户的渠道已经被冻结")
+        end
+      end
+
       @channel_user.update(last_sign_in_at: @channel_user.current_sign_in_at, current_sign_in_at: Time.now)
       @api_key = @channel_user.api_key
     else
