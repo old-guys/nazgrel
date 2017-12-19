b28030b964723e831c003fc3881abed63d44a826
francis jiang <jhjguxin@gmail.com>
2017-12-19 14:37:24 +0800

    [db] create report_channel_shop_newer

---
+++ app/models/reports/report_channel_shop_newer.rb
@@ -0,0 +1,2 @@
+class ReportChannelShopNewer < ApplicationRecord
+end
---
+++ db/migrate/20171218103105_create_report_channel_shop_newers.rb
@@ -0,0 +1,22 @@
+class CreateReportChannelShopNewers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :report_channel_shop_newers, comment: "渠道新增店主数据" do |t|
+      t.date :report_date, comment: "报表日期"
+      t.bigint :channel_id, comment: "渠道ID"
+      t.integer :stage_1_grade_platinum, comment: "00:00-9:00 白金店主数"
+      t.integer :stage_1_grade_gold, comment: "00:00-9:00 黄金店主数"
+      t.integer :stage_2_grade_platinum, comment: "09:00-18:00 白金店主数"
+      t.integer :stage_2_grade_gold, comment: "09:00-18:00 黄金店主数"
+      t.integer :stage_3_grade_platinum, comment: "18:00-24:00 白金店主数"
+      t.integer :stage_3_grade_gold, comment: "18:00-24:00 黄金店主数"
+      t.integer :month_grade_platinum, comment: "本月累计白金店主数"
+      t.integer :month_grade_gold, comment: "本月累计黄金店主数"
+      t.integer :year_grade_platinum, comment: "本年累计白金店主数"
+      t.integer :year_grade_gold, comment: "本年累计黄金店主数"
+
+      t.timestamps
+    end
+    add_index :report_channel_shop_newers, :report_date
+    add_index :report_channel_shop_newers, :channel_id
+  end
+end
