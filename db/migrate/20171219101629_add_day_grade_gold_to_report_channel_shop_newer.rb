17e898bd5de3a8bb7a560043f58c1585e6ff2d18
francis jiang <jhjguxin@gmail.com>
2017-12-27 17:33:34 +0800

    [migrate] Add comment to report_channel_shop_newer remove dirty code

--- db/migrate/20171219101629_add_day_grade_gold_to_report_channel_shop_newer.rb
+++ db/migrate/20171219101629_add_day_grade_gold_to_report_channel_shop_newer.rb
@@ -1,6 +1,6 @@
 class AddDayGradeGoldToReportChannelShopNewer < ActiveRecord::Migration[5.1]
   def change
-    add_column :report_channel_shop_newers, :day_grade_platinum, :integer
-    add_column :report_channel_shop_newers, :day_grade_gold, :integer
+    add_column :report_channel_shop_newers, :day_grade_platinum, :integer, comment: "本月累计白金店主数"
+    add_column :report_channel_shop_newers, :day_grade_gold, :integer, comment: "本月累计黄金店主数"
   end
 end
