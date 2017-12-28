f252deac9f7f11eedf6aad5db09d81e12adedc0a
francis jiang <jhjguxin@gmail.com>
2017-12-28 12:04:01 +0800

    Adjust dev report view

--- app/controllers/dev/report/shop_activities_controller.rb
+++ app/controllers/dev/report/shop_activities_controller.rb
@@ -2,6 +2,8 @@ class Dev::Report::ShopActivitiesController < Dev::Report::BaseController
   include ActionSearchable
 
   def index
+    params[:report_date] ||= Date.today.to_s
+
     @report_shop_activities = ReportShopActivity.preload(shop: :shopkeeper).where(report_date: params[:report_date])
 
     @report_shop_activities = filter_by_pagination(relation: @report_shop_activities, default_per_page: 50)
