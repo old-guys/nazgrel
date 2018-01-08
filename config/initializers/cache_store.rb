a9a3e755b18c0b8fffaca5c0b0ca9e9d28be00d8
francis jiang <jhjguxin@gmail.com>
2018-01-08 16:12:20 +0800

    Fix setting cache_store in initializer not working #29489
    https://github.com/rails/rails/issues/29489

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,12 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.configure do
-  redis_conf = SERVICES_CONFIG['redis']
-  config.cache_store = :redis_store, {
-    host: redis_conf['host'],
-    port: redis_conf['port'],
-    db: redis_conf['cache_db'],
-    password: redis_conf['password'],
-    expires_in: redis_conf['expires_in'] || 5.days
-  }
-end
+# https://github.com/rails/rails/issues/29489
+redis_conf = SERVICES_CONFIG['redis']
+Rails.cache = ActiveSupport::Cache.lookup_store(:redis_store, {
+  host: redis_conf['host'],
+  port: redis_conf['port'],
+  db: redis_conf['cache_db'],
+  password: redis_conf['password'],
+  expires_in: redis_conf['expires_in'] || 5.days
+})
