4c4b0286b3b805a15fd31d0a4a6bd19e880bf6d0
francis jiang <jhjguxin@gmail.com>
2018-01-11 18:09:26 +0800

    [report] Implement shop ecn report

---
+++ app/reports/shop_ecn/calculations.rb
@@ -0,0 +1,25 @@
+module ShopEcn::Calculations
+  def calculate(shop: )
+    _shopkeeper = shop.shopkeeper
+    _parent_shopkeeper = _shopkeeper.try(:parent)
+
+    ancestry_rate = if _parent_shopkeeper.try(:descendant_size).to_i > 1
+      _shopkeeper.descendant_size.to_f / _parent_shopkeeper.descendant_size.to_f
+    else
+      nil
+    end
+
+    {
+      ecn_count: _shopkeeper.descendant_size,
+      ancestry_rate: ancestry_rate,
+      ecn_grade_platinum_count: _shopkeeper.descendant_grade_platinum_size,
+      ecn_grade_gold_count: _shopkeeper.descendant_grade_gold_size,
+      children_count: _shopkeeper.children_size,
+      children_grade_platinum_count: _shopkeeper.children_grade_platinum_size,
+      children_grade_gold_count: _shopkeeper.children_grade_gold_size,
+      indirectly_descendant_count: _shopkeeper.indirectly_descendant_size,
+      indirectly_descendant_grade_platinum_count: _shopkeeper.indirectly_descendant_grade_platinum_size,
+      indirectly_descendant_grade_gold_count: _shopkeeper.indirectly_descendant_grade_gold_size
+    }
+  end
+end
---
+++ app/reports/shop_ecn/update_report.rb
@@ -0,0 +1,86 @@
+class ShopEcn::UpdateReport
+  class << self
+    def update_report(shops: , force_update: false, interval_time: 30.minutes)
+      _records = ReportShopEcn.where(
+        shop: shops
+      ).find_each.to_a
+
+      _time = Time.now
+
+      shops.each {|shop|
+        _record = _records.find{|record|
+          record.shop_id == shop.id
+        }
+
+        if _record.blank?
+          _records << ReportShopEcn.new(
+            shop: shop,
+            channel: shop.channel
+          )
+        end
+      }
+
+      _records.each {|_record|
+        _record.shop = shops.find{|s|
+          s.id == _record.shop_id
+        }
+
+        next if _record.shop.blank?
+        next if _record.shop.shopkeeper.blank?
+
+        next if not force_update and _record.persisted? and (_record.updated_at + interval_time) >= _time
+        logger.info "update report for shop_id: #{_record.shop_id}"
+
+        _report = ShopEcn::UpdateReport.new(
+          record: _record
+        )
+
+        _report.perform
+      }
+    end
+
+    def insert_to_partial_shops(id: )
+      _ids = Array.wrap(id).uniq
+      return if _ids.blank?
+
+      _key = ShopEcn::UpdateReport::SHOP_IDS_CACHE_KEY
+      $redis.SADD(_key, _ids)
+    end
+  end
+  include ShopEcn::Calculations
+  include ReportLoggerable
+
+  SHOP_IDS_CACHE_KEY = "shop_ecn_report_shop_ids"
+
+  attr_accessor :shop, :record, :result
+
+  def initialize(record: )
+    self.record = record
+
+    self.shop = record.shop
+  end
+
+  def perform
+    begin
+      process
+
+      write
+    rescue => e
+      logger.warn "update report failure #{e}, record: #{record.try(:attributes)}"
+    end
+  end
+
+  private
+  def process
+    @result = calculate(shop: shop)
+
+    record.assign_attributes(
+      @result
+    )
+  end
+  def write
+    record.changed? && record.save
+  end
+
+  private
+end
