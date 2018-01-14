9078036d843c47bb12542701b649b039c52b139a
francis jiang <jhjguxin@gmail.com>
2018-01-14 14:13:45 +0800

    Update 'api/open_mobile/dashboard/index' interface

--- app/controllers/concerns/api/open_mobile/action_ownable.rb
+++ app/controllers/concerns/api/open_mobile/action_ownable.rb
@@ -7,18 +7,18 @@ module Api::OpenMobile::ActionOwnable
   private
   def set_shops
     @permit_shops = if params[:shop_id].present?
-      _record = Shop.find_by(id: params[:shop_id])
+      @permit_shop = Shop.find_by(id: params[:shop_id])
 
-      _record.present? ? _record.descendant_entities : Shop.none
+      @permit_shop.present? ? @permit_shop.descendant_entities : Shop.none
     else
       Shop.all
     end
   end
   def set_shopkeepers
     @permit_shopkeepers = if params[:shop_id].present?
-      _record = Shopkeeper.find_by(shop_id: params[:shop_id])
+      @permit_shop = Shop.find_by(id: params[:shop_id])
 
-      _record.present? ? _record.descendant_entities : Shopkeeper.none
+      @permit_shop.present? ? @permit_shop.shopkeeper.descendant_entities : Shopkeeper.none
     else
       Shopkeeper.all
     end
--- app/views/api/open_mobile/dashboard/index.json.jbuilder
+++ app/views/api/open_mobile/dashboard/index.json.jbuilder
@@ -1,1 +1,13 @@
 json.(@result, *@result.keys)
+
+json.shop({})
+if @permit_shop
+  json.cache! ['api/open_mobile/dashboard#shop', @permit_shop] do
+    json.shop do
+      json.id @permit_shop.id
+      json.name @permit_shop.to_s
+      json.shopkeeper_user_name @permit_shop.shopkeeper.try :user_name
+      json.shopkeeper_user_phone @permit_shop.shopkeeper.try :user_phone
+    end
+  end
+end
