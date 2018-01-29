3b4c0618443cb4e75d25989f4e4bba75a055d459
francis jiang <jhjguxin@gmail.com>
2018-01-29 13:42:52 +0800

    Add `commission_income_amount` to shop_activity about report
    [migrate] add commission_income_amount desc to report_shop_activity
    [migrate] add commission_income_amount desc to report_city_shop_activity
    [migrate] add commission_income_amount desc to report_channel_shop_activity
    [migrate] add commission_income_amount desc to report_cumulative_shop_activity

---
+++ db/migrate/20180129034156_add_commission_income_amount_desc_to_report_city_shop_activity.rb
@@ -0,0 +1,11 @@
+class AddCommissionIncomeAmountDescToReportCityShopActivity < ActiveRecord::Migration[5.1]
+  def change
+    add_column :report_city_shop_activities, :commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :year_order_amount, comment: "店铺佣金"
+    add_column :report_city_shop_activities, :stage_1_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :commission_income_amount, comment: "00:00-9:00 店铺佣金"
+    add_column :report_city_shop_activities, :stage_2_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :stage_1_commission_income_amount, comment: "09:00-18:00 店铺佣金"
+    add_column :report_city_shop_activities, :stage_3_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :stage_2_commission_income_amount, comment: "18:00-24:00 店铺佣金"
+    add_column :report_city_shop_activities, :week_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :stage_3_commission_income_amount, comment: "本月店铺佣金"
+    add_column :report_city_shop_activities, :month_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :week_commission_income_amount, comment: "本月店铺佣金"
+    add_column :report_city_shop_activities, :year_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :month_commission_income_amount, comment: "本年店铺佣金"
+  end
+end
---
+++ db/migrate/20180129034447_add_commission_income_amount_desc_to_report_cumulative_shop_activity.rb
@@ -0,0 +1,10 @@
+class AddCommissionIncomeAmountDescToReportCumulativeShopActivity < ActiveRecord::Migration[5.1]
+  def change
+    add_column :report_cumulative_shop_activities, :day_0_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_60_order_amount, comment: "当天店铺佣金"
+    add_column :report_cumulative_shop_activities, :day_3_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_0_commission_income_amount, comment: "3天店铺佣金"
+    add_column :report_cumulative_shop_activities, :day_7_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_3_commission_income_amount, comment: "7天店铺佣金"
+    add_column :report_cumulative_shop_activities, :day_15_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_7_commission_income_amount, comment: "15天店铺佣金"
+    add_column :report_cumulative_shop_activities, :day_30_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_15_commission_income_amount, comment: "30天店铺佣金"
+    add_column :report_cumulative_shop_activities, :day_60_commission_income_amount, :decimal, precision: 11, scale: 3, default: 0, comment: "60天店铺佣金"
+  end
+end
