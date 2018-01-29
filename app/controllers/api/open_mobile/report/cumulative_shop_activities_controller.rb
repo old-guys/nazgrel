db45f3d7297feda9fc008e905f543d3c284309b1
francis jiang <jhjguxin@gmail.com>
2018-01-29 09:55:11 +0800

    [enhance] Cleanup controller 'api/open_mobile/report/cumulative_shop_activities'

--- app/controllers/api/open_mobile/report/cumulative_shop_activities_controller.rb
+++ app/controllers/api/open_mobile/report/cumulative_shop_activities_controller.rb
@@ -2,7 +2,7 @@ class Api::OpenMobile::Report::CumulativeShopActivitiesController < Api::OpenMob
   include ActionSearchable
 
   def index
-    @report_cumulative_shop_activities = ReportCumulativeShopActivity.preload(:shopkeeper)
+    @report_cumulative_shop_activities = ReportCumulativeShopActivity.all
     @report_cumulative_shop_activities = sort_records(relation: @report_cumulative_shop_activities)
 
     @report_cumulative_shop_activities = filter_by_pagination(relation: @report_cumulative_shop_activities)
