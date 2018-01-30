c1bbff4360f83e507ab54320533154948f43edd5
francis jiang <jhjguxin@gmail.com>
2018-01-30 14:22:50 +0800

    Add `shop_id` filter to `api/open_mobile/report/cumulative_shop_activitie`

--- app/controllers/api/open_mobile/report/cumulative_shop_activities_controller.rb
+++ app/controllers/api/open_mobile/report/cumulative_shop_activities_controller.rb
@@ -4,6 +4,9 @@ class Api::OpenMobile::Report::CumulativeShopActivitiesController < Api::OpenMob
   def index
     @report_cumulative_shop_activities = ReportCumulativeShopActivity.all
     @report_cumulative_shop_activities = sort_records(relation: @report_cumulative_shop_activities)
+    if params[:shop_id].present?
+      @report_cumulative_shop_activities = @report_cumulative_shop_activities.where(shop_id: params[:shop_id])
+    end
 
     @report_cumulative_shop_activities = filter_by_pagination(relation: @report_cumulative_shop_activities)
   end
