713235223a71550ba5199306603a5082b131ef25
francis jiang <jhjguxin@gmail.com>
2018-02-27 15:32:43 +0800

    [migrate] Change descendant_order_amount on report_cumulative_shop_activity [migrate] Change descendant_commission_income_amount on report_cumulative_shop_activity

---
+++ db/migrate/20180227072010_change_descendant_order_amount_on_report_cumulative_shop_activity.rb
@@ -0,0 +1,11 @@
+class ChangeDescendantOrderAmountOnReportCumulativeShopActivity < ActiveRecord::Migration[5.1]
+  def change
+    change_column :report_cumulative_shop_activities, :day_0_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_60_order_amount, comment: "当天下级订单金额"
+    change_column :report_cumulative_shop_activities, :day_3_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_0_descendant_order_amount, comment: "3天下级订单金额"
+    change_column :report_cumulative_shop_activities, :day_7_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_3_descendant_order_amount, comment: "7天下级订单金额"
+    change_column :report_cumulative_shop_activities, :day_15_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_7_descendant_order_amount, comment: "15天下级订单金额"
+    change_column :report_cumulative_shop_activities, :day_30_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_15_descendant_order_amount, comment: "30天下级订单金额"
+    change_column :report_cumulative_shop_activities, :day_60_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_30_descendant_order_amount, comment: "60天下级订单金额"
+    change_column :report_cumulative_shop_activities, :total_descendant_order_amount, :decimal, precision: 11, scale: 3, default: 0, after: :day_60_descendant_order_amount, comment: "总下级订单金额"
+  end
+end
