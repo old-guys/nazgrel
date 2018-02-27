3e3775e45bf096457ac56ca6d54cb5fc35ee0180
francis jiang <jhjguxin@gmail.com>
2018-02-27 10:45:19 +0800

    Implement `ShopkeeperStatusable` use data from `report_cumulative_shop_activity`

--- app/models/concerns/shopkeeper_statusable.rb
+++ app/models/concerns/shopkeeper_statusable.rb
@@ -22,41 +22,37 @@ module ShopkeeperStatusable
 
     def cache_status_keys
       [
-        "shopkeeper:#{id}:descendant_size",
-        "shopkeeper:#{id}:descendant_activation_size",
-        "shopkeeper:#{id}:descendant_grade_platinum_size",
-        "shopkeeper:#{id}:descendant_grade_gold_size",
-        "shopkeeper:#{id}:children_size",
-        "shopkeeper:#{id}:children_grade_platinum_size",
-        "shopkeeper:#{id}:children_grade_gold_size"
+        "#{status_cache_key}:descendant_size",
+        "#{status_cache_key}:descendant_activation_size",
+        "#{status_cache_key}:descendant_grade_platinum_size",
+        "#{status_cache_key}:descendant_grade_gold_size",
+        "#{status_cache_key}:children_size",
+        "#{status_cache_key}:children_grade_platinum_size",
+        "#{status_cache_key}:children_grade_gold_size"
       ].freeze
     end
   end
 
-  def descendant_entities_cache_key
-    @descendant_entities_cache_key ||= descendant_entities.cache_key
-  end
-
   def share_journal_count
-    Rails.cache.fetch("shopkeeper:#{id}:share_journal_count:raw", raw: true, expires_in: 30.minutes) {
+    Rails.cache.fetch("#{status_cache_key}:share_journal_count:raw", raw: true, expires_in: 30.minutes) {
       report_cumulative_shop_activity.try(:total_shared_count) || ShareJournal.where(shop_id: shop_id).count
     }.to_i
   end
 
   def view_journal_count
-    Rails.cache.fetch("shopkeeper:#{id}:view_journal_count:raw", raw: true, expires_in: 30.minutes) {
+    Rails.cache.fetch("#{status_cache_key}:view_journal_count:raw", raw: true, expires_in: 30.minutes) {
       report_cumulative_shop_activity.try(:total_view_count) || ViewJournal.where(shop_id: shop_id).count
     }.to_i
   end
 
   def descendant_size
-    @descendant_size ||= Rails.cache.fetch("shopkeeper:#{id}:descendant_size", raw: true) {
-      descendant_entities.size
+    @descendant_size ||= Rails.cache.fetch("#{status_cache_key}:descendant_size", raw: true) {
+      report_cumulative_shop_activity.try(:total_descendant_count) || descendant_entities.size
     }.to_i
   end
 
   def descendant_activation_size
-    @descendant_activation_size ||= Rails.cache.fetch("shopkeeper:#{id}:descendant_activation_size", raw: true) {
+    @descendant_activation_size ||= Rails.cache.fetch("#{status_cache_key}:descendant_activation_size", raw: true) {
       descendant_entities.where.not(order_number: nil).size
     }.to_i
   end
@@ -70,31 +66,31 @@ module ShopkeeperStatusable
   end
 
   def descendant_grade_platinum_size
-    @descendant_grade_platinum_size ||= Rails.cache.fetch("shopkeeper:#{id}:descendant_grade_platinum_size", raw: true) {
+    @descendant_grade_platinum_size ||= Rails.cache.fetch("#{status_cache_key}:descendant_grade_platinum_size", raw: true) {
       report_cumulative_shop_activity.try(:total_ecn_grade_platinum_count) || descendant_entities.grade_platinum.size
     }.to_i
   end
 
   def descendant_grade_gold_size
-    @descendant_grade_gold_size ||= Rails.cache.fetch("shopkeeper:#{id}:descendant_grade_gold_size", raw: true) {
+    @descendant_grade_gold_size ||= Rails.cache.fetch("#{status_cache_key}descendant_grade_gold_size", raw: true) {
       report_cumulative_shop_activity.try(:total_ecn_grade_gold_count) || descendant_entities.grade_gold.size
     }.to_i
   end
 
   def children_size
-    @children_size ||= Rails.cache.fetch("shopkeeper:#{id}:children_size", raw: true) {
-      children.size
+    @children_size ||= Rails.cache.fetch("#{status_cache_key}:children_size", raw: true) {
+      report_cumulative_shop_activity.try(:total_children_count) || children.size
     }.to_i
   end
 
   def children_grade_platinum_size
-    @children_grade_platinum_size ||= Rails.cache.fetch("shopkeeper:#{id}:children_grade_platinum_size", raw: true) {
+    @children_grade_platinum_size ||= Rails.cache.fetch("#{status_cache_key}:children_grade_platinum_size", raw: true) {
       report_cumulative_shop_activity.try(:total_children_grade_platinum_count) || children.grade_platinum.size
     }.to_i
   end
 
   def children_grade_gold_size
-    @children_grade_gold_size ||= Rails.cache.fetch("shopkeeper:#{id}:children_grade_gold_size", raw: true) {
+    @children_grade_gold_size ||= Rails.cache.fetch("#{status_cache_key}:children_grade_gold_size", raw: true) {
       report_cumulative_shop_activity.try(:total_children_grade_gold_count) || children.grade_gold.size
     }.to_i
   end
@@ -112,23 +108,27 @@ module ShopkeeperStatusable
   end
 
   def descendant_order_number
-    Rails.cache.fetch("shopkeeper:#{id}:#{descendant_entities_cache_key}:descendant_order_number", raw: true) {
-      descendant_entities.sum(:order_number)
+    Rails.cache.fetch("#{status_cache_key}:descendant_order_number", raw: true) {
+      report_cumulative_shop_activity.try(:total_descendant_order_number) || descendant_entities.sum(:order_number)
     }.to_i
   end
 
   def descendant_order_amount
-    Rails.cache.fetch("shopkeeper:#{id}:#{descendant_entities_cache_key}:descendant_order_amount", raw: true) {
-      descendant_entities.sum(:order_amount)
-    }
+    BigDecimal.new(Rails.cache.fetch("#{status_cache_key}:descendant_order_amount", raw: true) {
+      report_cumulative_shop_activity.try(:total_descendant_order_amount) || descendant_entities.sum(:order_amount)
+    })
   end
 
   def descendant_commission_income_amount
-    Rails.cache.fetch("shopkeeper:#{id}:#{descendant_entities_cache_key}:descendant_commission_income_amount", raw: true) {
-      descendant_entities.sum(:commission_income_amount)
-    }
+    BigDecimal.new(Rails.cache.fetch("#{status_cache_key}:descendant_commission_income_amount", raw: true) {
+      report_cumulative_shop_activity.try(:total_descendant_commission_income_amount) || descendant_entities.sum(:commission_income_amount)
+    })
   end
 
+  private
+  def status_cache_key
+    @status_cache_key ||= ActiveSupport::Cache.expand_cache_key([cache_key, report_cumulative_shop_activity])
+  end
   module ClassMethods
   end
 end
