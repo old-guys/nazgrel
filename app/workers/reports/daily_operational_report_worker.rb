d75e37835a51c16e6cdd79ef1d6437d0f567349d
francis jiang <jhjguxin@gmail.com>
2018-03-08 10:25:02 +0800

    Force reset yesterday daily_operational_report

--- app/workers/reports/daily_operational_report_worker.rb
+++ app/workers/reports/daily_operational_report_worker.rb
@@ -6,9 +6,20 @@ class DailyOperationalReportWorker
 
   def perform(*args)
     logger.info "start: args #{args}"
-    DailyOperational::UpdateReport.update_report(
-      report_date: Date.today
-    )
+    options = args.extract_options!
+    _type = options["type"] || "today"
+
+    case _type
+      when "today"
+        DailyOperational::UpdateReport.update_report(
+          report_date: Date.today
+        )
+      when "yesterday"
+        DailyOperational::UpdateReport.update_report(
+          report_date: Date.yesterday,
+          force_update: true
+        )
+    end
 
     logger.info "finished"
   end
