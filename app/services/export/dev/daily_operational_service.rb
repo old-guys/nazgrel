24db67cf8e48707e5cc337fab783e64a55634325
francis jiang <jhjguxin@gmail.com>
2018-03-16 11:29:46 +0800

    Adjust DailyOperationalService export field

--- app/services/export/dev/daily_operational_service.rb
+++ app/services/export/dev/daily_operational_service.rb
@@ -4,11 +4,15 @@ class Export::Dev::DailyOperationalService
   def report_fields
     %w(
       id report_date
-      total_shopkeeper_count shopkeeper_count activation_shopkeeper_count
-      activation_shopkeeper_rate view_count viewer_count shared_count
-      order_total_price order_count order_pay_price order_conversion_rate
-      order_total_price_avg commission_income_amount
-      activity_ticket_amount product_cost
+         total_shopkeeper_count shopkeeper_count activation_shopkeeper_count
+         activation_shopkeeper_rate view_count viewer_count shared_count
+         order_total_price order_count
+         income_coin use_coin
+         order_pay_price order_conversion_rate
+         sale_order_total_price sale_order_total_price_rate
+         shopkeeper_order_total_price shopkeeper_order_total_price_rate
+         order_total_price_avg commission_income_amount
+         activity_ticket_amount product_cost withdraw_amount
     )
   end
 
@@ -16,9 +20,14 @@ class Export::Dev::DailyOperationalService
     %w(
       # 报表日期
       累计店主数 店主数 活跃店主数
-      活跃店主比例 浏览量 访客数 访客数
-      订单总额 订单数 支付金额 订单转化率
-      平均每单金额 佣金 现金券 成本
+      活跃店主比例 浏览量 访客数 分享数
+      订单总额 订单数
+      芝蚂币收入 芝蚂币使用
+      支付金额 订单转化率
+      销售订单总额 销售订单总额占比
+      自购订单总额 自购订单总额占比
+      平均每单金额 佣金
+      现金券 成本 提现金额
     )
   end
 end
