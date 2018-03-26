a603e0e4a2910da89db1d90a888453ebf6723c95
francis jiang <jhjguxin@gmail.com>
2018-03-26 17:37:50 +0800

    [refactor] Improve ShopActivityReportWorker partial update

--- app/workers/reports/shop_activity_report_worker.rb
+++ app/workers/reports/shop_activity_report_worker.rb
@@ -25,24 +25,25 @@ class ShopActivityReportWorker
         # _ids = $redis.SPOP(_key, 100)
         _ids = $redis.SMEMBERS(_key)
         $redis.DEL(_key) if _ids.present?
-        _shops = Shop.preload(:shopkeeper).where(id: _ids)
 
         _ids.each_slice(500) {|ids|
+          _shops = Shop.preload(:shopkeeper).where(id: ids)
+
           ShopActivity::UpdateReport.update_report(
             shops: _shops,
             interval_time: 5.minutes
           )
+
+          CumulativeShopActivity::UpdateReport.insert_to_partial_shops(
+            id: _shops.map(&:id)
+          )
+
+          CityShopActivity::UpdateReport.insert_to_partial_city(
+            city: _shops.map{|record|
+              record.shopkeeper.try(:city)
+            }.compact.uniq
+          )
         }
-
-        CumulativeShopActivity::UpdateReport.insert_to_partial_shops(
-          id: _shops.map(&:id)
-        )
-
-        CityShopActivity::UpdateReport.insert_to_partial_city(
-          city: _shops.map{|record|
-            record.shopkeeper.try(:city)
-          }.compact.uniq
-        )
     end
 
     logger.info "finished"
