671ba81776bd83fa781be957156094dd326a6b0b
francis jiang <jhjguxin@gmail.com>
2018-04-02 11:50:02 +0800

    [refactor] freeze report `sum_fields` as static variable

--- app/reports/channel_shop_activity/calculations.rb
+++ app/reports/channel_shop_activity/calculations.rb
@@ -1,14 +1,24 @@
 module ChannelShopActivity::Calculations
+  extend ActiveSupport::Concern
+
+  included do
+    delegate :sum_fields, to: "self.class"
+  end
 
   def calculate(report_shop_activities: )
-    _sum_fields = ReportChannelShopActivity.stat_fields.map {|field|
-      Arel.sql("sum(`report_shop_activities`.`#{field}`) as #{field}")
-    }
-
     format_calculate_hash(
       result: report_shop_activities.pluck_h(
-        *_sum_fields
+        *sum_fields
       ).pop
     )
+  end
+
+  private
+  module ClassMethods
+    def sum_fields
+      @sum_fields ||= ReportChannelShopActivity.stat_fields.map {|field|
+        Arel.sql("sum(`report_shop_activities`.`#{field}`) as #{field}")
+      }.freeze
+    end
   end
 end
--- app/reports/cumulative_shop_activity/calculations.rb
+++ app/reports/cumulative_shop_activity/calculations.rb
@@ -1,4 +1,9 @@
 module CumulativeShopActivity::Calculations
+  extend ActiveSupport::Concern
+
+  included do
+    delegate :sum_fields, :total_sum_fields, to: "self.class"
+  end
 
   def calculate(shop_id: , date: )
     result = ReportCumulativeShopActivity.stat_cumulative_stages.each_with_object({}) {|stage, hash|
@@ -14,29 +19,38 @@ module CumulativeShopActivity::Calculations
 
   private
   def calculate_by_stage(stage: , shop_id: , date:)
-    _sum_fields = ReportCumulativeShopActivity.stat_categories.map {|field|
-      Arel.sql("sum(`report_shop_activities`.`#{field}`) as #{stage}_#{field}")
-    }
     dates = (stage.split("_").last.to_i).days.ago(date)..date
 
     format_calculate_hash(
       result: ReportShopActivity.where(
         report_date: dates,
         shop_id: shop_id
-      ).pluck_h(*_sum_fields).pop
+      ).pluck_h(*sum_fields(stage: stage)).pop
     )
   end
 
   def calculate_by_total(shop_id: , date:)
-    _sum_fields = ReportCumulativeShopActivity.stat_categories.map {|field|
-      :"total_#{field}"
-    }
-
     format_calculate_hash(
       result: ReportShopActivity.where(
         report_date: date,
         shop_id: shop_id
-      ).pluck_h(*_sum_fields).pop
+      ).pluck_h(*total_sum_fields).pop
     )
+  end
+  private
+  module ClassMethods
+    def sum_fields(stage: )
+      @sum_fields ||= {}
+
+      @sum_fields[stage] ||= ReportCumulativeShopActivity.stat_categories.map {|field|
+        Arel.sql("sum(`report_shop_activities`.`#{field}`) as #{stage}_#{field}")
+      }.freeze
+    end
+
+    def total_sum_fields
+      @total_sum_fields ||= ReportCumulativeShopActivity.stat_categories.map {|field|
+        :"total_#{field}"
+      }.freeze
+    end
   end
 end
