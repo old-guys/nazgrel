db8546d8879760274243b669d9494fc54a1d19ca
francis jiang <jhjguxin@gmail.com>
2018-04-23 12:33:24 +0800

    Adjust 'dev/report/shops' add user_phone field

--- app/services/export/dev/shop_service.rb
+++ app/services/export/dev/shop_service.rb
@@ -3,7 +3,8 @@ class Export::Dev::ShopService
 
   def report_fields
     %w(
-      id name shopkeeper.user_name shopkeeper.province shopkeeper.city
+      id name shopkeeper.user_name shopkeeper.user_phone
+      shopkeeper.province shopkeeper.city
       shopkeeper.created_at shopkeeper.user_grade_i18n
       shopkeeper.order_number shopkeeper.order_amount
       shopkeeper.shopkeeper_order_number shopkeeper.shopkeeper_order_amount
@@ -33,7 +34,8 @@ class Export::Dev::ShopService
 
   def report_head_names
     %w(
-      id 店铺名称 店主姓名 省份 城市
+      id 店铺名称 姓名 手机号
+      省份 城市
       创建时间 店铺等级
       订单数 订单金额
       自购订单数 自购订单金额
